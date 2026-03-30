589acc8ec87ffd8286a6f9af1d60660124bca4db
chungmu21 <agentantares@gmail.com>
2026-03-30 19:54:10 +0800

    basics

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("Hello World!")
+//console.log("i like pizza")
+//window.alert("Hello World!")
+//window.alert("this is a test")
+
+document.getElementById("myH1").textContent = "Hello World!";
+document.getElementById("myP").textContent = "pizza!";
+//this is a comment
