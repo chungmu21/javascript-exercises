24735b4f41f5c90b27f5b7b128836ee9be97749f
chungmu21 <agentantares@gmail.com>
2026-03-31 11:05:35 +0800

    numbers and string

--- script.js
+++ script.js
@@ -3,6 +3,26 @@
 //window.alert("Hello World!")
 //window.alert("this is a test")
 
-document.getElementById("myH1").textContent = "Hello World!";
-document.getElementById("myP").textContent = "pizza!";
-//this is a comment
+//document.getElementById("myH1").textContent = "Hello World!";
+//document.getElementById("myP").textContent = "pizza!";
+//this is a comment sample
+
+let x ;
+x = 1234;
+
+console.log(x);
+
+let y ;
+y = 4321
+
+console.log(y)
+
+let age = 25;
+let price = 10;
+let gpa = 1.4;
+let favoriteFood = "sushi"
+
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+console.log(`You like ${favoriteFood}`);
