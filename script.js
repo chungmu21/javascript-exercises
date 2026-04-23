3bd0c14d83d3a4b20113b19ac6a614f074fb6aa2
chungmu21 <agentantares@gmail.com>
2026-04-23 20:09:32 +0800

    arithmethic operators

--- script.js
+++ script.js
@@ -3,25 +3,25 @@
 // 1. declaration   let x;
 // 2. assignment    x = 100;
 
-let x;
-x = 1234;
+//let x;
+//x = 1234;
 
-console.log(x);
+//console.log(x);
 
-let y;
-y = 4321
+//let y;
+//y = 4321
 
-console.log(y)
+//console.log(y)
 
-let age = 25;
-let price = 10;
-let gpa = 1.4;
-let favoriteFood = "sushi"
+//let age = 25;
+//let price = 10;
+//let gpa = 1.4;
+//let favoriteFood = "sushi"
 
-console.log(`You are ${age} years old`);
-console.log(`The price is $${price}`);
-console.log(`Your gpa is: ${gpa}`);
-console.log(`You like ${favoriteFood}`);
+//console.log(`You are ${age} years old`);
+//console.log(`The price is $${price}`);
+//console.log(`Your gpa is: ${gpa}`);
+//console.log(`You like ${favoriteFood}`);
 
 
 //console.log("Hello World!")
@@ -33,7 +33,16 @@ console.log(`You like ${favoriteFood}`);
 //document.getElementById("myP").textContent = "pizza!";
 //this is a comment sample
 
+//arithmethic operators = operands ( values, Variables, etc.)
+//                          operators ( + - * / )
+//                             ex. 11 = x + 5);
 
+let people = 31;
+//people = people + 1;
+//people = people - 1;
+//people = people * 2;
+people = people / 2;
+console.log(people);
 
 
 
